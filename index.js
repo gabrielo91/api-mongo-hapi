fbbecc89a1fb49cc06e04f7d95d4b3dafae7a038
Gabriel Ortega <gabrielo_9103@hotmail.com>
2020-04-12 20:04:26 -0500

    linter added

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-const PORT = process.env.PORT || 3442;
-const Hapi = require("@hapi/hapi");
+'use strict'
+
+const PORT = process.env.PORT || 3442
+const Hapi = require('@hapi/hapi')
+
+const server = Hapi.server({
+  port: PORT,
+  host: 'localhost'
+})
 
 const init = async () => {
-  const server = Hapi.server({
-    port: PORT,
-    host: "localhost",
-  });
-
   server.route({
-    method: "Get",
-    path: "/",
+    method: 'Get',
+    path: '/',
     handler: (request, h) => {
-      return "hello world";
-    },
-  });
+      return 'hello world'
+    }
+  })
 
-  await server.start();
-  console.log(`Listen on ${PORT}`);
-};
+  await server.start()
+  console.log(`Listen on ${PORT}`)
+}
 
-process.on("unhandledRejection", () => {
-  console.log(err);
-  process.exit(1);
-});
+process.on('unhandledRejection', (err) => {
+  console.log(err)
+  process.exit(1)
+})
 
-init();
+init()
